b966d941ffb10114a889263bb7b229ec33e957d7
swarup-1 <swarupkadoli9@gmail.com>
2023-02-23 17:55:18 +0530

    Single Product Design & Responsiveness

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import Bag from "./Pages/Bag"
+import SingleProduct from "./Pages/SingleProduct";
 
 function App() {
   return (
     <div >
 <h1 className="text-red-700">Hello asos team !!!</h1>
-<Bag/>
+<SingleProduct />
+{/* <Bag/> */}
     </div>
   );
 }
