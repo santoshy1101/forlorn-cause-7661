6708462e6401142ba8ed949b87a3bb48ca3ddb1c
santosh <styadav018@gmail.com>
2023-02-24 09:10:25 +0530

    added men page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import Bag from "./Pages/Bag"
+import HomePage from "./Pages/HomePage";
 
 function App() {
   return (
     <div >
 <h1 className="text-red-700">Hello asos team !!!</h1>
 <Bag/>
+<HomePage/>
     </div>
   );
 }
