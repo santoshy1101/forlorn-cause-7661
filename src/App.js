c820979a1e3ac9e1320cfd8ba3e531b456d31054
swarup-1 <swarupkadoli9@gmail.com>
2023-02-26 14:20:00 +0530

    Routes

--- src/App.js
+++ src/App.js
@@ -5,19 +5,14 @@ import SingleProduct from "./Pages/SingleProduct";
 import Checkout from "./Pages/Checkout";
 import CheckoutComponent from "./Components/CheckoutComponent";
 import Footer from "./Components/Footer/Footer";
+import Navbar from "./Components/Navbar";
+import AllRoutes from "./Components/AllRoutes";
 
 function App() {
   return (
     <div >
-
-
-<Men/>
-{/* <SingleProduct /> */}
-{/* <Bag/> */}
-{/* <Checkout/> */}
-
-      <Footer/>
-
+      <Navbar />
+      <AllRoutes />
     </div>
   );
 }
