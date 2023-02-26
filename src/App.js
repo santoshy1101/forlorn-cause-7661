5c81d88355b7e73939b91f8e96adcb11df95b3d1
santosh <styadav018@gmail.com>
2023-02-26 22:35:26 +0530

    navber responsiveness

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import Footer from "./Components/Footer/Footer";
-<<<<<<< HEAD
-
-=======
->>>>>>> 3d5b6421f4c899bbae41bb56ca81ffb6756d6075
 import AllRoutes from "./Components/AllRoutes";
 import Navbar from "./Components/Navbar/Navbar";
 
