e76f052ad915def16d246bbffeec358072b739af
PranayJadhao <pranayjadhao95@gmail.com>
2023-02-26 23:44:55 +0530

    adding

--- src/Components/BagItem.jsx
+++ src/Components/BagItem.jsx
@@ -20,28 +20,29 @@ export function BagItems(el) {
     <Box >
       <Flex
         key={el._id}
-        width={{ base: "100%", md: "95%", lg: "90%" }}
+      //  width={{ base: "100%", md: "95%", lg: "90%" }} 
         margin={"auto"}
-        justify={"space-around"}
-        alignItems={"center"}
+        // justify={"space-between"}
+        alignItems={"flex-start"}
         borderBottom={"1px solid #999999"}
         mt={2}
         mb={2}
         pb={5}
         pt={5}
+      
       >
-        <Box pr='3'  >
+        <Box pr='0'  >
           <Image
             src={el.image}
-             width={{ base: "100px", md: "120px", lg: "120px" }}
-            m={0}
+             width={ "30%"}
+             m={0}
           />
         </Box>
 
-        <Box>
+        <Box  ml={"-44"}>
           <Text
             fontSize={{ base: 16, md: 20, lg: 20 }}
-            width={{ base: "120px", md: "250px", lg: "400px" }}
+            // width={{ base: "100%", md: "50%", lg: "100%" }}
           >
             {el.product}
           </Text>
@@ -61,8 +62,8 @@ export function BagItems(el) {
           </Text>
           </s>
         </Box>
-        <Box>
-          <CloseButton
+        <Box >
+          <CloseButton 
             display={{ base: "block", md: "block", lg: "block" }}
             color={"#5b5b5b"}
             onClick={() => deleteHandler(_id)}
