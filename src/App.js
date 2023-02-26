3d5b6421f4c899bbae41bb56ca81ffb6756d6075
swarup-1 <swarupkadoli9@gmail.com>
2023-02-26 22:28:14 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Footer from "./Components/Footer/Footer";
-import Navbar from "./Components/Navbar";
 import AllRoutes from "./Components/AllRoutes";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
